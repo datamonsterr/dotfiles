fe8548f3708e4df0670b472249536d2e74a6701d
Pham Dat <phamthanhdat17092004@gmail.com>
2022-04-08 22:13:49 +0700

    starship!!!!!!!!!!!!!!

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,16 @@
+starship init fish | source
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 set fish_greeting
 set fish_color_search_match --background="#44447a"
 
+alias ls="exa -la"
+alias nv="nvim7"
+alias cat="bat"
+alias md="mkdir"
+alias rmf="rm -rf"
+alias s="sudo"
+alias se="sudoedit"
+
+set PATH /home/dat/.cargo/bin $PATH
