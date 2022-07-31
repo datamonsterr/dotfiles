8125671b1a805b5ba4a805feb47cffe1907904c5
datamonsterr <phamthanhdat17092004@gmail.com>
2022-07-31 15:27:01 +0700

    Update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,10 @@
 starship init fish | source
 zoxide init fish | source
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
 set fish_greeting
 set fish_color_search_match --background="#44447a"
 
